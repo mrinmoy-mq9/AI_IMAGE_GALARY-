bd978595d700f29e0cd44071369d35644af11245
mrinmoy-mq9 <130278747+mrinmoy-mq9@users.noreply.github.com>
2023-08-13 12:45:34 +0530

    Add files via upload

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,185 @@
+import React, { useState } from 'react';
+import { useNavigate, useNavigation } from 'react-router-dom';
+
+import { preview , download} from '../assets';
+
+import { getRandomPrompt} from "../util";
+import { FormField , Loader } from '../components';
+const CreatePost = () => {
+
+
+  const navigate = useNavigate();
+  const [ form , setForm] = useState({
+    name:'mrinmoy',
+    prompt:'',
+    photo:''
+  });
+
+  const [genImg , setGenImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isSurpriseMe, setIsSurprisMe] = useState(true);
+
+  const generatImage = async ()=>{
+    if(form.prompt){
+      try {
+        setGenImg(true);com
+        const response = await fetch('https://dallem.onrender.com/api/v1/dalle',{
+          method:'POST',
+          headers:{
+            'content-type':'application/json',
+          },
+          body:JSON.stringify({prompt:form.prompt})
+        })
+        
+
+        const data = await response.json();
+
+        setForm({...form , photo:`data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert(error);
+      }finally{
+        setGenImg(false);
+      }
+    }else{
+      alert('please enter a prompt')
+    }
+  }
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    if(form.prompt && form.name){
+      setLoading(true);
+
+      try {
+        const response = await fetch("https://dallem.onrender.com/api/v1/post/",{
+          method:"POST",
+          headers:{
+            "Content-Type":"application/json",
+          },
+          body:JSON.stringify(form)
+        })
+        console.log(JSON.stringify(form));
+        await response.json();
+        navigate("/");
+      } catch (error) {
+        alert(error);
+      } finally{
+
+        setLoading(false);
+
+      }
+    }else{
+      alert(" enter your prompt please...");
+    }
+  }
+
+  const handleChangeN = (e) => {
+    setForm({...form , name: e.target.value})
+  }
+
+  const handleChangeP = (e) => {
+    setForm({...form , prompt: e.target.value})
+  }
+
+  const handleSurpriseMe = () => {
+    const randomP = getRandomPrompt(form.prompt);
+    setForm({ ...form , prompt:randomP});
+  }
+
+  return (
+   <section className=''>
+     <div className='max-w-7xl'>
+        <h1 className='font-extrabold font-mono -mt-4 text-center text-[#5c289c] text-[32px]'>
+          LET YOUR CRAZZY IMAGINATION take WINGS
+        </h1>
+        <p className='text-[16px] mt-2 text-center font-mono text-violet-600'>Build your FUTURE with AI APPLICATION</p>
+
+      </div>
+
+      <div className=' flex  justify-center'>
+        
+      <form className='max-w-3xl mt-6  ' onSubmit={handleSubmit}> 
+        <FormField
+          lebelName='your name'
+          name="name"
+          type='text'
+          placeholder='john Doe'
+          handleChange={handleChangeN} 
+          value={form.name} 
+        />
+
+        <FormField 
+          lebelName='prompt'
+          name="prompt"
+          type='text'
+          placeholder='your name is thios'
+          handleChange={handleChangeP} 
+          value={form.prompt}   
+          isSurpriseMe
+          handleSurpriseMe= {handleSurpriseMe}
+        />
+
+        <div className="relative bg-grey-50 border border-grey-300 text-grey-900 text-sm 
+            rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 mt-4 p-3 flex justify-center
+            item-center "
+        >
+              {form.photo ? (
+                <img
+                src={form.photo}
+                alt={form.photo}
+                className='w-full h-full object-contain'
+              />
+             
+                
+              ):(
+              
+                <img
+                src={preview}
+                alt='prevew'
+                className='w-9/12 h- 9/12object-contain opacity-40'
+              />
+              )}
+
+              {genImg && (
+                <div className='flex inset-0 z-0 justify-center item-center
+                bg-[rgba(0,0,0,0.5)] rounded-lg absolute'>
+                  <Loader/>
+                </div>
+              )}
+          </div>
+   
+          <div className='mt-5 flex gap-5'>
+            <button 
+              type='button'
+              onClick={generatImage}  
+              className='text-white bg-green-700 font-medium rounded-md
+              text-sm w-full  px-5 py-2.5 text-center' 
+            >
+              {genImg ? "generating.....":"generate"}
+            </button>
+          </div>
+
+          <div className='mt-10'>
+            <p className='text-[#666e75] text-[14px] mt-2'>you have created the imag you want, you can share it with other in the comunity</p>
+            <button
+              type='submit'
+              className='w-full text-white font-medium text-sm bg-[#6469ff] rounded-md px-5 py-2.5 text-center mt-3'
+            >
+              { loading ? 'sharing.....' : 'share with comunity'}
+            </button>
+          </div>
+        
+        </form>
+      </div>
+      
+
+    
+
+   
+
+    
+   </section>
+  )
+}
+
+export default CreatePost
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,130 @@
+import React, {useState , useEffect} from 'react';
+import { logo } from '../assets';
+import {Link} from "react-router-dom"
+
+import { Card , Loader, FormField } from '../components';
+//import { set } from 'mongoose';
+
+
+const RenderCard = ({data , title}) => {
+  if(data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />)
+  }
+
+  return (
+    <h2 className=' font-bold uppercase text-xl text-[#66449ff'>{title}</h2>
+  )
+}
+const Home = () => {
+
+  const [loading , setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState('new image');
+  const [searchResult ,setSearchResults] = useState('');
+  const [searchTimeout ,setSearchTimeout] = useState(null);
+
+  useEffect(()=>{
+   
+    const fetchPosts = async() => {
+      setLoading(true);
+
+      try {
+        const response = await fetch("https://dallem.onrender.com/api/v1/post/",{
+          method:"GET",
+          headers:{
+            "Content-Type":"application/json",
+          },
+
+        })
+        console.log(response.ok)
+        if(response.ok){
+          const result = await response.json();
+          console.log(result["data"])
+          setAllPosts(result.data);
+        }
+      } catch (error) {
+        alert(error);
+      }finally{
+        setLoading(false);
+      }
+    }
+    fetchPosts();
+   
+  },[]);
+
+  const handleSearchChange = (e) =>{
+    clearTimeout(searchTimeout);
+
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(()=>{
+        const searchResult = allPosts.filter((item)=> item.name.toLowerCase().includes(searchText.toLowerCase()) || 
+        item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        
+        setSearchResults(searchResult);
+      
+      } , 500)
+    );
+    
+  }
+  return (
+    <section className='max-w-7xl mx-auto'>
+      <div >
+        <h1 className='font-extrabold text-center font-mono  text-[#7330b2] text-[32px]'>
+          AI IMAGE GALARY
+        </h1>
+        <p className='text-[16px] mt-2 font-mono text-center text-violat-700'>An Era of <span className='Text-md font-serif font-bold text-green-600'>ARTIFICIAL</span> creativity </p>
+
+        
+      </div>
+
+      <div className='mt-16   max-w-7xl'>
+        <FormField
+          lebelName={"Search post"}
+          tyoe='text'
+          name='text'
+          placeholder='search post'
+          value ={searchText}
+          handleChange={handleSearchChange}
+        />
+      </div>
+      
+      <div className='mt-10'>
+        { loading ? (
+          <div className='flex justify-center item-center'>
+            <Loader/>
+          </div>
+        ) : 
+        <>
+          { searchText && (
+            <h2 className='text-[#3439d5] mb-3'>
+              Showing result for || <span className='text-[#4c32cc]'>{searchText}</span>
+            </h2>
+          )}
+
+          <div className='grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-3'>
+            { searchText ? (
+              <RenderCard 
+                data={allPosts}
+                title={'no search'}              
+              />
+            ) :
+            (
+              <RenderCard  
+                data={allPosts}
+                title='no post found'
+              />
+            )
+            }
+
+          </div>
+        </>
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Home
