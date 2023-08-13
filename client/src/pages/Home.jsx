c74a70052cc851a39d94c032657ae3f5e45607f9
mrinmoy-mq9 <130278747+mrinmoy-mq9@users.noreply.github.com>
2023-08-13 12:00:41 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8000/api/v1/post/",{
+        const response = await fetch("https://dallem.onrender.com/api/v1/post/",{
           method:"GET",
           headers:{
             "Content-Type":"application/json",
